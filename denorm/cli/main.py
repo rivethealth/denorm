6dcb8922aae816db0a3f9a70495c3cda56c55f3b
Paul Draper <paulddraper@gmail.com>
2021-04-07 03:39:37 -0600

    Add version command

--- denorm/cli/main.py
+++ denorm/cli/main.py
@@ -1,18 +1,10 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(dest="command")
-
-create_key_parser = subparsers.add_parser("create-agg")
-create_key_parser.add_argument("--schema", default="-")
-create_key_parser.add_argument("--output", default="-")
-
-create_join_parser = subparsers.add_parser("create-join")
-create_join_parser.add_argument("--schema", default="-")
-create_join_parser.add_argument("--output", default="-")
+from ..version import __version__
 
 
 def main():
+    parser = _create_parser()
     args = parser.parse_args()
 
     if args.command == "create-agg":
@@ -23,3 +15,34 @@ def main():
         from .create_join import cli
 
         cli(args)
+
+
+def _create_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}",
+        help="show version and exit",
+    )
+
+    subparsers = parser.add_subparsers(dest="command")
+
+    _add_create_agg_command(subparsers)
+    _add_create_join_command(subparsers)
+
+    return parser
+
+
+def _add_create_agg_command(subparsers):
+    parser = subparsers.add_parser("create-agg")
+    parser.add_argument("--schema", default="-")
+    parser.add_argument("--output", default="-")
+
+
+def _add_create_join_command(subparsers):
+    parser = subparsers.add_parser("create-join")
+    parser.add_argument("--schema", default="-")
+    parser.add_argument("--output", default="-")
