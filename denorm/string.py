fb19b66583d1102888d56891840382f2ae7a79f5
Paul Draper <paulddraper@gmail.com>
2022-04-14 09:08:51 -0600

    Improve indent
    Don't indent empty lines

--- denorm/string.py
+++ denorm/string.py
@@ -4,4 +4,4 @@ _LINE_RE = re.compile("^|(?<=\n)")
 
 
 def indent(str: str, count: int) -> str:
-    return "\n".join("  " * count + line for line in str.split("\n"))
+    return "\n".join("  " * count + line if line else "" for line in str.split("\n"))
